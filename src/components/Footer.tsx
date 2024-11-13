788e701d333aaf026d71ce12589e9b1265d98dda
Arvind Prakash <arvind687@gmail.com>
2024-11-14 00:54:17 +0530

    Update Git repo link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="mt-auto">
             <hr className="mt-5" />
             <div className="flex justify-center p-8 pb-20 sm:p-10">
-                <span>Made with NextJS and Tailwind.  <CustomLink href="https://github.com/DrNightmare/blog" target="_blank">Link to Github repo</CustomLink></span>
+                <span>Made with NextJS and Tailwind.  <CustomLink href="https://github.com/DrNightmare/blog-v2" target="_blank">Link to Github repo</CustomLink></span>
             </div>
         </footer>
     );
