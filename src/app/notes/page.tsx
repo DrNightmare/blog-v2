0df9be47fe003f3d3b030162dfe0e3d6640ae01a
Arvind Prakash <arvind687@gmail.com>
2024-12-22 17:03:29 +0530

    Add travel tracking note

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,14 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { getNotes } from "../utils";
+import CustomLink from "@/components/CustomLink";
+
+const components = {
+    a: (props: any) => (
+        <CustomLink {...props}>
+            {props.children}
+        </CustomLink>
+    )
+};
 
 export default function Notes() {
     const notes = getNotes();
@@ -12,10 +21,10 @@ export default function Notes() {
                     <span className="mb-6 text-center">A collection of initial thoughts, brainstorming, and in-progress learnings.</span>
                     {notes.slice().reverse().map((note, index) => (
                         <div className="not-prose my-2" key={note.slug}>
-                            <div className="mb-2">
+                            <div className="mb-5">
                                 <p className="text-lg font-medium text-crimson">{note.metadata.title}</p>
                                 <span className="text-sm font-thin text-gray-600">{note.metadata.date}</span>
-                                <article className="prose"><MDXRemote source={note.content} /></article>
+                                <article className="prose"><MDXRemote source={note.content} components={components} /></article>
                             </div>
                             {index < notes.length - 1 && <hr />}
                         </div>
